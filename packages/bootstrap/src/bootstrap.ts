70a0bb9a7d437cfb7a3ca50fe7aeea5378cdc74d
silverbirder <silverbirder@gmail.com>
2020-12-16 22:30:00 +0000

    🙂 Try vaadin/router!

--- packages/bootstrap/src/bootstrap.ts
+++ packages/bootstrap/src/bootstrap.ts
@@ -1,10 +1,19 @@
-const components: string[] = ["x-component"];
+import {Router} from '@vaadin/router';
+import XComponent from './x-component';
+customElements.define("x-component", XComponent);
 
-components
-.filter((component: string) => document.querySelector(component) !==  null)
-.map((component: string) => {
-  import(`./${component}.js`).then(x => {
-    // @ts-ignore
-    customElements.define(component, x.default);
-  });
-});
+const outlet = document.getElementById('outlet');
+const router = new Router(outlet);
+router.setRoutes([
+  {path: '/', component: 'x-component'},
+]);
+
+// const components: string[] = ["x-component"];
+// components
+// .filter((component: string) => document.querySelector(component) !==  null)
+// .map((component: string) => {
+//   import(`./${component}.js`).then(x => {
+//     // @ts-ignore
+//     customElements.define(component, x.default);
+//   });
+// });
