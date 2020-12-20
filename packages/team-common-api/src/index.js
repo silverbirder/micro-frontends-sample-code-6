8edc8a77e15638a7809732e7aac2fb9d4ced1c65
silverbirder <silverbirder@gmail.com>
2020-12-20 22:25:11 +0000

    🙂 Start mupti packages cli.

--- packages/team-common-api/src/index.js
+++ packages/team-common-api/src/index.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
-require('fastify')().get('/manifest.json', async (request, reply) => {
-    reply.type('application/json').code(200)
-    return require('./modules/map')['envMap']
-}).listen(process.env.PORT || 3000)
+require('fastify')()
+.register(require('fastify-cors'), { 
+    origin: (origin, cb) => {
+        cb(null, true)
+        return
+      }
+})
+.get('/manifest.json', async (request, reply) => {
+  reply.type('application/json').code(200)
+  return require('./modules/map')['envMap']
+})
+.listen(process.env.PORT || 3000)
